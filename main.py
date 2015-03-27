39540815300758e70ccaddb444f445e4d8a3c8a4
17reynobry <17reynobry@washk12.org>
2015-03-27 09:13:19 -0600

    so um what now

--- main.py
+++ main.py
@@ -6,16 +6,8 @@ print("Hello, Awesome!")
 #WOWOWOOWOOWOWOWOW
 #I have no idea what im doing!!!!!!
 
-
-
-
-
-
-
-
-
-
-
-
+#Harry Dunne: Just when I thought you couldn't possibly be
+# any dumber, you go and do something like this... and
+# totally redeem yourself!
 
 qwerty says hi
